9a3b9391e6b614620d22e032b01c19f30ebcd45f
tarfa <tarfa.mansour@gmail.com>
2017-11-02 20:22:55 +0300

    send
    send

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
 var num = 12;
 var marker;
+var infowindow;
+var map;
+
 var locations = [{
     title: 'Danube Hypermarket Al Malqa',
     details: "Place1",
@@ -47,21 +50,19 @@ function errorGoogleMap() {
 }
 // Maps api asynchronous load code here.
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 24.782661,
             lng: 46.629109
         },
         zoom: num
     });
+    infowindow = new google.maps.InfoWindow();
     var i;
-    var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
     for (i = 0; i < locations.length; i++) {
-
         var position = locations[i].location;
         var title = locations[i].title;
-
         marker = new google.maps.Marker({
             map: map,
             position: position,
@@ -70,10 +71,9 @@ function initMap() {
             id: i
         });
         // var markers = [];
-        this.markers = ko.observableArray("");
+        this.markers = ko.observableArray();
         markers.push(marker);
         locations[i].marker = marker;
-
         bounds.extend(marker.position);
         marker.addListener('click', minFun(marker));
 
@@ -81,8 +81,8 @@ function initMap() {
     //to solve (Don't make functions within a loop.)
     function minFun(marker) {
         return function() {
-            if (largeInfowindow.marker != marker) {
-                populateInfoWindow2(marker, largeInfowindow, marker.position.lat(), marker.position.lng());
+            if (infowindow.marker != marker) {
+                populateInfoWindow2(marker, infowindow, marker.position.lat(), marker.position.lng());
                 marker.setAnimation(google.maps.Animation.BOUNCE);
                 // largeInfowindow.open(map, marker);
                 // this.setAnimation(google.maps.Animation.BOUNCE);
@@ -92,19 +92,19 @@ function initMap() {
     }
 
     function populateInfoWindow(marker, infowindow) {
-        if (infowindow.marker != marker) {
+        if (infowindow.marker == marker) {
             infowindow.marker = marker;
             marker.setAnimation(google.maps.Animation.BOUNCE);
             trunOffBounce(marker);
             infowindow.setContent('<div>' + marker.title + '</div>');
             infowindow.open(map, marker);
             marker.addListener('closeclick', function() {
-                infowindow.setMarker(null);
-                // infowindow.open(map ,marker);
+                // infowindow.setMarker(null);
+                infowindow.open(map, marker);
             });
         }
     } //populateInfoWindow
-} //initMap
+}
 
 function LocationObj(title, lat, lng, details, marker) {
     this.title = ko.observable(title);
@@ -119,7 +119,6 @@ function LocationObj(title, lat, lng, details, marker) {
         else if (event.srcElement) target = event.srcElement;
         for (var i = 0; i < locations.length; i++) {
             if (locations[i].title == title) {
-                var infowindow = new google.maps.InfoWindow();
                 var lat = locations[i].location.lat;
                 var lng = locations[i].location.lng;
                 populateInfoWindow2(locations[i].marker, infowindow, lat, lng);
@@ -133,44 +132,40 @@ function LocationObj(title, lat, lng, details, marker) {
 function model() {
     var self = this;
     self.locationobjs = ko.observableArray("");
-    self.allarray = ko.observableArray("");
+    self.allarray = ko.observableArray([]);
     self.query = ko.observable("");
     self.filteredLocations = ko.computed(function() {
         var filter = self.query().toLowerCase();
-        // {DisableAllMarckers();}
         if (!filter) {
+            if (locations[0].marker) {
+
+                ShowAllMarckers();
+                infowindow.close();
+            }
             return self.locationobjs();
-            // DisableAllMarckers();
         } else {
             return ko.utils.arrayFilter(self.locationobjs(), function(item) {
                 var isMatching = item.title().toLowerCase().indexOf(filter) !== -1;
-                if (isMatching) {
+                if (isMatching === true) {
                     var allarray = self.filteredLocations();
-                    if (allarray.length > 4) {
-                        DisableAllMarckers();
-                    }
-                    if (allarray.length == 1) {
-                        ShowAllMarckers();
-                        var infowindow = new google.maps.InfoWindow();
-                        var found = GetMarker(allarray[0].title());
-                        DeleteAllexceptMe(allarray[0].title());
-                        var lat1 = allarray[0].lat();
-                        var lng1 = allarray[0].lng();
-                        populateInfoWindow2(found, infowindow, lat1, lng1);
-                        found.setAnimation(google.maps.Animation.BOUNCE);
-                        trunOffBounce(found);
-                        // ShowAllMarckers();
-                        // infowindow.close();
-                    }
-                } else { //Not matching
-                    // DisableAllMarckers(); // it gave some issue
+                    var found = GetMarker(item.title());
+                    DeleteAllexceptMe(found);
+                    var lat1 = allarray[0].lat();
+                    var lng1 = allarray[0].lng();
+                    hint(found);
+                    populateInfoWindow2(found, infowindow, lat1, lng1);
+                    return isMatching;
+                    // });
+                } else {
+                    DisableAllMarckers();
+                    infowindow.close();
+
                 }
 
-                return isMatching;
-            });
-        }
-    }); //end filteredLocations
+            }); //end filteredLocations
 
+        }
+    });
 }
 
 var mymodel = new model();
@@ -193,49 +188,57 @@ function loaddata() {
 
 function populateInfoWindow2(marker, infowindow, lan, lng) {
 
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        var phone, address, name;
-        var apiURL = 'https://api.foursquare.com/v2/venues/';
-        var foursquareClientID = 'WPGVLH0N5HSOOVKJUASAX0N4KWP0PWZDOOYJZLDHYVIJEH3S';
-        var foursquareSecret = 'GAQGBLH5Q3YEJHFJVCBUDSDLJL5RYMJEK3DQYJEQWMALQ1KW';
-        var venueFoursquareID = "20161016";
-        var foursquareURL = apiURL + 'search?v=' + venueFoursquareID + '&ll=' + lan + ',' + lng + '&intent=checkin&' + 'client_id=' + foursquareClientID + '&client_secret=' + foursquareSecret;
-        $.ajax({
-            url: foursquareURL,
-            success: function(data) {
-                phone = data.response.venues[0].contact.phone;
-                address = data.response.venues[0].location.address;
-                name = data.response.venues[0].name;
-                this.phone = ko.observable(phone);
-                if (phone === "" || phone === null || typeof phone == 'undefined') {
-                    phone = "Not avilable";
-                }
-                if (address === "" || address === null || typeof address == 'undefined') {
-                    address = "Not avilable";
-                }
-                if (name === "" || name === null || typeof name == 'undefined') {
-                    name = "Not avilable";
-                }
-                infowindow.setContent('<div>' + 'Name :' + name + '/' + marker.title + '<br>' + 'PHONE(#) :' + phone + ' Address : ' + address + '</div>');
-                infowindow.open(map, marker);
-                name = ""; address = ""; phone = ""; // to fix one issue
-                marker.addListener('closeclick', function() {
-                name = ""; address = ""; phone = "";// to fix one issue
-                });
-            },
-            error: function(error) {
-                alert("location details are not available now , please try again.");
+    infowindow.marker = marker;
+    var phone, address, name;
+    var apiURL = 'https://api.foursquare.com/v2/venues/';
+    var foursquareClientID = 'WPGVLH0N5HSOOVKJUASAX0N4KWP0PWZDOOYJZLDHYVIJEH3S';
+    var foursquareSecret = 'GAQGBLH5Q3YEJHFJVCBUDSDLJL5RYMJEK3DQYJEQWMALQ1KW';
+    var venueFoursquareID = "20161016";
+    var foursquareURL = apiURL + 'search?v=' + venueFoursquareID + '&ll=' + lan + ',' + lng + '&intent=checkin&' + 'client_id=' + foursquareClientID + '&client_secret=' + foursquareSecret;
+    $.ajax({
+        url: foursquareURL,
+        success: function(data) {
+            phone = data.response.venues[0].contact.phone;
+            address = data.response.venues[0].location.address;
+            name = data.response.venues[0].name;
+            this.phone = ko.observable(phone);
+            if (phone === "" || phone === null || typeof phone == 'undefined') {
+                phone = "Not avilable";
             }
-        });
+            if (address === "" || address === null || typeof address == 'undefined') {
+                address = "Not avilable";
+            }
+            if (name === "" || name === null || typeof name == 'undefined') {
+                name = "Not avilable";
+            }
+            infowindow.setContent('<div>' + 'Name :' + name + '/' + marker.title + '<br>' + 'PHONE(#) :' + phone + ' Address : ' + address + '</div>');
+            infowindow.open(map, marker);
+            name = "";
+            address = "";
+            phone = ""; // to fix one issue
+            marker.addListener('closeclick', function() {
+                name = "";
+                address = "";
+                phone = ""; // to fix one issue
+            });
+        },
+        error: function(error) {
+            alert("location details are not available now , please try again.");
+        }
+    });
 
-    } //END IF
-
+    //END IF
 
 } //populateInfoWindow
 function trunOffBounce(marker) {
     setTimeout(function() {
         marker.setAnimation(null);
+    }, 3000);
+}
+
+function hint(marker) {
+    setTimeout(function() {
+        marker.setVisible(true);
     }, 3000);
 }
 // passing title to get Marker details -
@@ -248,31 +251,28 @@ function GetMarker(title) {
     }
     return mk;
 }
-//passing title and remove other markers
-function DeleteAllexceptMe(title) {
-    for (i = 0; i < locations.length; i++) {
-        if (locations[i].title == title) { //Do nothing
-            locations[i].marker.setVisible(true);
-        } else {
-            // locations[i].marker.setMap(null);
-            locations[i].marker.setVisible(false);
-            // locations[i].marker.setMap(null);
-            // locations[i].marker=null;
-        }
+
+function DeleteAllexceptMe(locas) { //take array of loc and make it visible
+    var mke;
+    for (var i = 0; i < locas.length; i++) {
+      mke[i].setVisible(true);
     }
-    return locations;
+    return locas;
 }
 
 function ShowAllMarckers() {
+    var mke;
     for (i = 0; i < locations.length; i++) {
-        locations[i].marker.setVisible(true);
+        mke = GetMarker(locations[i].title);
+        mke.setVisible(true);
     }
 
 }
-
 function DisableAllMarckers() {
+    var mke;
     for (i = 0; i < locations.length; i++) {
-        locations[i].marker.setVisible(false);
-    }
+        mke = GetMarker(locations[i].title);
+        mke.setVisible(false);
+        }
 
 }
